f69ef39108225d49244ff90d1799b01c3b06e504
UMARNAE <166687903+UMARNAE@users.noreply.github.com>
2024-07-03 20:20:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// const navigation = document.querySelector('.navigation');
+// const stickyTrigger = document.querySelector('.sticky-trigger');
+
+// window.addEventListener('scroll', () => {
+//     if (window.scrollY > stickyTrigger.offsetTop) {
+//       navigation.classList.add('sticky');
+//     } else {
+//       navigation.classList.remove('sticky');
+//     }
+// });    
+
+<!-- JavaScript -->
+  const navigation = document.querySelector('.navigation');
+  const stickyTrigger = document.querySelector('.sticky-trigger');
+  
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > stickyTrigger.offsetTop) {
+      navigation.classList.add('sticky');
+    } else {
+      navigation.classList.remove('sticky');
+    }
+  });
